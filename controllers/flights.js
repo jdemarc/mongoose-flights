2f7680cfca79533c31619c29ba8b9c6fcfebbac0
joseph <josephjohndemarco@gmail.com>
2020-08-27 02:29:22 -0400

    Fix ticket/flight relationship.

--- controllers/flights.js
+++ controllers/flights.js
@@ -38,32 +38,11 @@ function newFlight(req, res) {
 function show(req, res) {
     Flight.findById(req.params.id, function(err, flight) {
 
-        //Sort flight destinations by arrival time.
-        flight.destinations.sort((a, b) => a.arrival.getTime() - b.arrival.getTime());
+        Ticket.find({ flight: flight._id} , function (err, attachedTickets) {
 
-        //Retrieve all possible flight destinations
-        destinationEnums = flight.schema.path('destinations.airport').enumValues;
-
-        //Convert flight.destinations to an array containing only the airports.
-        let usedAirports = flight.destinations.map(f => f.airport);
-
-        //Add the departure airport to list of airports.
-        usedAirports.push(flight.airport);
-
-        //Filter usedAirports to display allowedDestinations
-        let allowedDestinations = destinationEnums.filter(a => !usedAirports.includes(a));
-
-        //Ticket.find({ flight: flight._id})...
-        Ticket.find({}, function(err, createdTickets) {
-
-            Ticket.find({ flight: flight._id} , function (err, attachedTickets) {
-            //     console.log(attachedTickets);
-
-                res.render('flights/show', { flight, title: 'Flight Details', 
-                                            createdTickets, attachedTickets });
-            });
-            
+            res.render('flights/show', { flight, title: 'Flight Details', attachedTickets });
         });
+            
     });
 }
 
--- routes/tickets.js
+++ routes/tickets.js
@@ -4,6 +4,6 @@ const ticketsCtrl = require('../controllers/tickets');
 
 router.get('/flights/:id/tickets/new', ticketsCtrl.new);
 router.post('/flights/:id/tickets', ticketsCtrl.create);
-router.delete('/flights/:id/tickets', ticketsCtrl.deleteAllTickets);
+router.delete('/tickets', ticketsCtrl.delete);
 
 module.exports = router;
